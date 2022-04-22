da41923565086a27fcfaa363bd5f9661a0a8a9ec
V <goodros39@gmail.com>
2022-04-22 20:42:57 +0300

    updates

---
+++ public/robots.txt.php
@@ -0,0 +1,9 @@
+<?
+header('Content-type: text/plain');
+?>
+User-agent: *
+
+Allow: /css/
+Allow: /cdn-cgi/scripts/
+
+Sitemap: https://<?= $_SERVER['SERVER_NAME']; ?>/sitemap.xml
--- public/sitemap.xml.php
+++ public/sitemap.xml.php
@@ -2,6 +2,7 @@
 header('Content-type: application/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 $last_mod = '2022-04-01T00:00:00+01:00';
+$articles = require(__DIR__.'/../assets/php/articles.php');
 ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
@@ -10,27 +11,32 @@ $last_mod = '2022-04-01T00:00:00+01:00';
         <priority>1.0</priority>
     </url>
     <url>
+        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/automatic-instagram-likes</loc>
+        <lastmod><?= $last_mod; ?></lastmod>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/buy-instagram-comments</loc>
+        <lastmod><?= $last_mod; ?></lastmod>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/buy-instagram-followers</loc>
+        <lastmod><?= $last_mod; ?></lastmod>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/buy-instagram-likes</loc>
+        <lastmod><?= $last_mod; ?></lastmod>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/buy-instagram-views</loc>
+        <lastmod><?= $last_mod; ?></lastmod>
+        <priority>1.0</priority>
+    </url>
+    <url>
         <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/contact</loc>
-        <lastmod><?= $last_mod; ?></lastmod>
-        <priority>1.0</priority>
-    </url>
-    <url>
-        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/free-instagram-followers</loc>
-        <lastmod><?= $last_mod; ?></lastmod>
-        <priority>1.0</priority>
-    </url>
-    <url>
-        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/get-started</loc>
-        <lastmod><?= $last_mod; ?></lastmod>
-        <priority>1.0</priority>
-    </url>
-    <url>
-        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/get-started</loc>
-        <lastmod><?= $last_mod; ?></lastmod>
-        <priority>1.0</priority>
-    </url>
-    <url>
-        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/pricing</loc>
         <lastmod><?= $last_mod; ?></lastmod>
         <priority>1.0</priority>
     </url>
@@ -44,5 +50,21 @@ $last_mod = '2022-04-01T00:00:00+01:00';
         <lastmod><?= $last_mod; ?></lastmod>
         <priority>1.0</priority>
     </url>
-
+    <url>
+        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/privacy-policy</loc>
+        <lastmod><?= $last_mod; ?></lastmod>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/blog/</loc>
+        <lastmod><?= $last_mod; ?></lastmod>
+        <priority>1.0</priority>
+    </url>
+    <? foreach ($articles as $article) : ?>
+        <url>
+            <loc>https://<?= $_SERVER['SERVER_NAME']; ?>/blog/<?= $article['slug']; ?></loc>
+            <lastmod><?= $last_mod; ?></lastmod>
+            <priority>1.0</priority>
+        </url>
+    <? endforeach; ?>
 </urlset>
