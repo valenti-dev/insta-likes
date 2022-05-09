bfa2943b5140ec0ebb0d2580c12758285873edf1
V <goodros39@gmail.com>
2022-05-09 12:38:28 +0300

    emails added

---
+++ public/emails/completed.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome in Followerus</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://<?= $_SERVER['SERVER_NAME']; ?>/fonts/gilroy/stylesheet.css">
+    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+</head>
+<body style="margin: 0; padding: 0; background-color: #4A4A4A;">
+<div style="background-color: #ffffff; padding: 36px 42px 34px; border-radius: 10px; margin: 40px auto; width: 457px; max-width: 100%; box-sizing: border-box; box-shadow: 5px 15px 40px rgba(30, 35, 66, 0.15);">
+    <img alt="" src="https://<?= $_SERVER['SERVER_NAME']; ?>/img/emails/completed.jpg" style="width: 278px; max-width: 100%; display: block; margin: 0 auto 40px;">
+    <h1 style="font-weight: 900; font-size: 24px; line-height: 1.235; color: #252726; text-align: center; margin: 0 auto 14px; font-family: Gilroy, sans-serif; letter-spacing: 0.05em; text-transform: uppercase;">Automatic Likes completed</h1>
+    <div style="line-height: 1.575; color: #252726; text-align: center; font-size: 16px; font-weight: 700; font-family: Gilroy, sans-serif; margin: 0 auto 25px; color: rgba(92, 190, 114, 0.54);">
+        Username
+    </div>
+    <div style="padding: 18px; box-sizing: border-box; border-radius: 31px; line-height: 1.575; text-align: center; color: #292E43; font-size: 16px; font-family: Gilroy, sans-serif; margin: 0 auto 25px; border: 1px solid #CCCCCC;">100 Instagram Likes</div>
+    <a href="#" style="display: flex; max-width: 100%; width: 184px; box-sizing: border-box; align-items: center; justify-content: space-between; padding: 0 20px; background-color: #FFC266; cursor: pointer; color: #ffffff; height: 60px; border-radius: 30px; text-decoration: none; line-height: 1; margin: 0 auto 20px;">
+        <span style="font-family: Gilroy, sans-serif; font-size: 20px; font-weight: 600; margin: auto; color: #ffffff;">Extend</span>
+    </a>
+    <div style="text-align: center;">
+        <a href="#" style="color: #9297AB; text-decoration-line: none; line-height: 1.465; font-size: 14px; font-weight: 500; font-family: Gilroy, sans-serif;">Unsubscribe</a>
+    </div>
+</div>
+</body>
+</html>
