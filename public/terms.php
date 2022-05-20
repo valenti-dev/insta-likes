793f1dba6ba36ae60daa2b6b3ed6b1cd43246418
V <goodros39@gmail.com>
2022-05-20 12:19:16 +0300

    terms upd

--- public/terms.php
+++ public/terms.php
@@ -6,10 +6,12 @@ $description = 'Terms of Service. Our online social media marketing platform Lik
 <? include '../assets/php/head.php'; ?>
     <section class="title_sect">
         <div class="wrap">
-            <h1 class="row1"><span>Terms of Service</span> Agreement</h1>
+            <h1 class="row1"><span>Terms and conditions:</span><br> Rules, refund, and privacy</h1>
+            <!--
             <div class="row2">
                 Our Rules
             </div>
+            -->
         </div>
     </section>
 <?
@@ -21,51 +23,94 @@ $styles['title_sect'] = [
 <section class="text_sect">
     <div class="wrap">
         <div class="row1">
+            <h2>1. Definition</h2>
             <p>
-                Last Updated : February 03, 2022<br>
-                Thanks for using our products and services (“Services”). The Services are offered by <a href="https://likewave.io">Likewave</a>. By accessing this website, you are agreeing to be bound by this website’s <b>Terms and Conditions of Use</b>, all applicable laws, and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials included in this website are protected by appropriate copyright and trademark law.
+                <b>Likewave</b>- service at the address <a href="/">likewave.io</a> operated by Qloyd OчU registration code 16257994. <b>Address</b>: Harju maakond, Tallinn, Kesklinna linnaosa, Roseni tn 13, 10111, Estonia, in accordance with laws of Estonia.<br>
+                <b>Warranty</b> - The option of financial compensation for damaged or missing products.
             </p>
-            <h2>Use License</h2>
             <p>
-                Permission is granted to temporarily download one copy of the materials (information or software) on Likewave’s website for only personal, non-commercial transitory viewing. This is the grant of a license, not a transfer of title, and under this license, you may not:
+                By <b>service</b>, we mean a website.
             </p>
-            <ul>
-                <li>modify or copy the materials;</li>
-                <li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
-                <li>attempt to decompile or reverse engineer any software contained on Likewaves website;</li>
-                <li>remove any copyright or other proprietary notations from the materials; or</li>
-                <li>transfer the materials to another person or “mirror” the materials on any other server.</li>
-            </ul>
             <p>
-                This license shall automatically terminate if you violate any of these restrictions and may be terminated by Likewave at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
+                <b>Buyer</b> - a physical or corporate person who requires our services.
+            </p>
+            <p>
+                <b>Ordering</b> - Providing appropriate data and payment.
+            </p>
+            <p>
+                <b>The usage data</b> is information automatically accumulated by the Service's infrastructure (e.g. the duration of a page visit).
+            </p>
+            <p>
+                <b>You</b> mean the person gaining access or making use of the Services and/or the company that the individual is accessing or using the Services on behalf of, as applicable.
             </p>
             <div class="color_bg">
-                <h2>Disclaimer</h2>
-                <p>
-                    The materials on Likewave’s website are provided “as is”. Likewave makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Likewave does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet website or otherwise relating to such materials or on any sites linked to this site.
-                </p>
-                <h2>Limitations</h2>
-                <p>
-                    In no event shall Likewave or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on Likewave’s Internet site, even if Likewave or a Likewave’s authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not permit limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
-                </p>
+                <h2>2. General rules</h2>
+                <ul>
+                    <li>You must notify our team in case of any kind of technical error. Free Instagram Followers can only be allowed once to try out our services.</li>
+                    <li>Once you have uploaded the profile photo, submitted the accurate information, your profile is unlocked for everyone, you can proceed with the order.</li>
+                    <li>If a user declares a dispute with Paypal, his/her account will be blacklisted, as well as suspended.</li>
+                    <li>There will be an announcement if there are some changes to our terms.</li>
+                </ul>
             </div>
-            <h2>Billing</h2>
+            <h2>3. Using private data sources</h2>
             <p>
-                Payments on Likewave are processed using a secure and verified payment gateway, with no sensitive financial information input through our website. The secure method of accepting payments is processed using 256-bit SSL encryption. By purchasing our services you are explicitly agreeing that you clearly understand and agree to what you are purchasing and will not file a fraudulent dispute via your credit card or debit card institution. Furthermore, you have authorization to use the credit card, debit card, or any other payment source used to make the purchase. Upon a fraudulent attempt to file a dispute or chargeback, we receive the right, if necessary, to reset all followers and likes, terminate your account, and/or permanently ban your IP address. We will also submit all evidence on file to the other party. Users acknowledge chargebacks, disputes, or payment reversals will not be carried out prior to discussing the situation with our support team.
-            </p>
-            <h2>Revisions and Errata</h2>
-            <p>
-                The materials appearing on Likewave’s website could include technical, typographical, or photographic errors. Likewave does not warrant that any of the materials on its website are accurate, complete, or current. Likewave may make changes to the materials contained on its website without notice. Likewave does not, however, make any commitment to update the materials.
+                <b>Attention</b>: We may disclose certain information in connection with the Service to these third-party vendors.<br>
+                <b>Interest</b>: With this new feature, we will continue to give you the same great features and benefits of the Service.<br>
+                <b>Desire</b>: We will continue to work with these third party vendors for your protection.<br>
+                <b>Action</b>: We will never share any of your personal information other than what is strictly required for the service, and all third-party vendors must adhere to our strict privacy policies.
             </p>
             <div class="color_bg">
-                <h2>Links</h2>
+                <h2>4. Refund</h2>
+                <ul>
+                    <li>Any order that cannot be fulfilled because we have been unable to contact you or because of an inaccurate profile, we will refund your payment.</li>
+                    <li>If you provide inaccurate information that caused the situation that the purchase was delivered to the wrong person, we will not issue a refund.</li>
+                    <li>You might be eligible for a full refund for products paid for the day before shipment if you did not get it promptly.</li>
+                    <li>You can ask for a refund once within one day of the date of purchase, as long as it's within the warranty period.</li>
+                    <li>We don't provide refunds if you order from more than one service unless it's a product that we can give to another customer.</li>
+                    <li>Warranty, as well as available data, can be found in the item description.</li>
+                </ul>
+            </div>
+            <h2>5. Terms of delivery</h2>
+            <ul>
+                <li>As soon as the transaction is successful, the service will start processing.</li>
+                <li>Your service will not begin if payment has been delayed or canceled.</li>
+                <li>If the service specifies a 24-hour turnaround time, the process starts as soon as payment has gone through. You will receive an order to the mail you specify and then make payment.</li>
+                <li>
+                    Please contact us using the form or email at <a href="mailto:contact@likewave.io">contact@likewave.io</a> if you have not received the service.
+                </li>
+            </ul>
+            <div class="color_bg">
+                <h2>6. Responsibility</h2>
+                <ul>
+                    <li>It is possible to have your order processed once your account is authenticated to the public, and you upload a profile photo.</li>
+                    <li>The service may not look or work quite as expected when we try to use it. This is because each person needs a different level of service.</li>
+                    <li>The customer's responsibility is to make sure that the necessary steps are taken after receiving the order.</li>
+                    <li>We cannot ensure an accurate performance description if order quantity is greater than the maximum limit.</li>
+                    <li>Mailing address of the cardholder: Harju maakond, Tallinn, Kesklinna linnaosa, Roseni tn 13, 10111, Estonia.</li>
+                </ul>
+            </div>
+            <h2>7. Privacy policy</h2>
+            <ul>
+                <li>We guarantee that the email address will never be shared with third parties.</li>
+                <li>We reserve the right to email you notifications. These will only be sent to the address provided when creating a ticket or order.</li>
+                <li>All your data is strictly secured, guaranteeing that no third parties will ever know who you are. Your anonymity is fully insured at all times without fail.</li>
+                <li>To protect the users' private data, we only allow direct account access to active customers who have already confirmed phone/email ownership and hold encryption keys securely. Your information will never be given to a third party.</li>
+                <li>Third parties can't tap payments due to the encryption you're using.</li>
+                <li>In rare cases where your card fails or doesn't work, we'll let you know as soon as possible by e-mail and on our website so that you don't lose your money.</li>
+                <li>We use encryption and SSL to protect your privacy. Your card data is not stored or transmitted, and the only exception is through our payment gateways. We don't keep any of your details, such as card information.</li>
+            </ul>
+            <div class="color_bg">
+                <h2>Changes to this Privacy Policy</h2>
                 <p>
-                    Likewave has not reviewed all of the sites linked to its Internet website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Likewave of the site. Use of any such linked website is at the user’s own risk.
+                    You can always check here for the latest version of our Privacy Policy.
                 </p>
-                <h2>Privacy Policy</h2>
                 <p>
-                    You have to read our <a href="https://likewave.io/privacy-policy">Privacy Policy</a> if you agree to use https://likewave.io
+                    We will notify you by email before any changes to this Privacy Policy. Additionally, we will post a notice on our Service and update the "Last Updated" date in this Privacy Policy before the change occurs.
                 </p>
+                <p>
+                    We ask that you check this Privacy Policy from time to time for changes. Changes will happen as soon as the new updates are added to this page.
+                </p>
+                <img alt="Changes to this Privacy Policy" src="/img/text_sect/image1.png">
             </div>
         </div>
     </div>
